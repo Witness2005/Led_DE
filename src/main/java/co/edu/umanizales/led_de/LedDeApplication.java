192ef0feac4585e2cc949c0d08a6e61bcfa92d65
Witness2005 <127411214+Witness2005@users.noreply.github.com>
2023-05-08 18:05:04 -0500

    Add files via upload
    Primer commit

---
+++ src/main/java/co/edu/umanizales/led_de/LedDeApplication.java
@@ -0,0 +1,13 @@
+package co.edu.umanizales.led_de;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedDeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LedDeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/umanizales/led_de/LedDeApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.umanizales.led_de;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedDeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
